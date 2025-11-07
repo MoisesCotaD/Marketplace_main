191025e49dc88d181ad3c12b6f8914a36ca49538
MoisesCotaD <cotadelpradomoisesito@gmail.com>
2025-11-07 08:23:14 -0800

    Agregue la funcion contact y la navegación a templates

--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Item, Category
-from django.shortcuts import get_list_or_404
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def home(request):
@@ -12,3 +12,22 @@ def home(request):
         'categories': categories
     }
     return render(request,'store/home.html', context)
+
+def contact(request):
+    context = {
+        'msg': 'quieres otros productos contactame!'
+    }
+
+    return render(request, 'store/contact.html', context)
+
+def detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(Category=item.category,
+                                        is_sold=False).exclude(pk=pk)[0:3]
+    
+    context={
+        'item':item,
+        'related_items': related_items
+    }
+
+    return render(request, 'store/item.html', context)
